b9dbaa349aec1f4ec00a58286941830087dca17a
PinkMuscle <chck3685@naver.com>
2019-05-06 08:24:04 +0900

    Add MySQL

---
+++ sql.js
@@ -0,0 +1,22 @@
+const mysql = require("mysql")
+const dblogin = require("./tokens/dblogin.json");
+
+const db = mysql.createConnection({
+	host: dblogin.host,
+	user: dblogin.user,
+	password: dblogin.password,
+	database: dblogin.database,
+});
+
+db.connect(function(err) {
+	if (err) {
+	  console.error('error connecting: ' + err.stack);
+	  return;
+	}
+
+	console.log('connected as id ' + db.threadId);
+});
+
+
+exports.mysql = mysql;
+exports.db = db;
