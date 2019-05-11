5c1ee0efb68d131933f163a32aa4f6219be1a4e2
PinkMuscle <chck3685@naver.com>
2019-05-12 08:46:26 +0900

    Add weekReader

---
+++ util/weekReader.js
@@ -0,0 +1,13 @@
+const fs = require("fs");
+
+const data = fs.readFileSync("./week.json", "utf8");
+let weekInfo = JSON.parse(data);
+
+exports.get = function (key) {
+    return weekInfo[key];
+}
+
+exports.set = function (key, value) {
+    weekInfo[key] = value;
+    fs.writeFileSync("./week.json", JSON.stringify(weekInfo), { encoding: 'utf8', flag: 'w' });
+}
